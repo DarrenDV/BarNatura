338cfae1383c6af9fb3fc4563961a631002d4076
G-mecha <maydayfan11@gmail.com>
2020-10-08 10:40:20 +0200

    Imported old assets

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject target;
+
+    float speed = 3.5f;
+    float minFov = 35f;
+    float maxFov = 100f;
+    float sensitivity = 23f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement function
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(target.transform.position, transform.up, Input.GetAxis("Mouse X") * speed);
+            transform.RotateAround(target.transform.position, transform.right, Input.GetAxis("Mouse Y") * -speed);
+        }
+
+        // Zoom function
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
