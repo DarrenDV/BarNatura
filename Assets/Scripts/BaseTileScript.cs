6ea412ff6a494c6f6440d80dec529b05aeb8f352
DarrenDV <darrendevre@hotmail.com>
2020-10-08 11:19:48 +0200

    Base tile variables, please give feedback if variables could have been different or should be in another script

---
+++ Assets/Scripts/BaseTileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTileScript : MonoBehaviour
+{
+    [Header("Default tile production variables")]
+    [SerializeField] protected float oxygenProduction; 
+    [SerializeField] protected float pollutionProduction;
+
+    [Header("Tile pollution state variables")]
+    protected float maxPollutedPercentage = 100f;
+    [SerializeField] protected float pollutedPercentage;  
+    
+    [Header("Checks if the tile is occupied")]
+    [SerializeField] protected bool occupied;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This could be in another function or method
+        if(pollutedPercentage >= maxPollutedPercentage){
+            //Make tile uninhabitable or toxic.
+        }
+    }
+}
