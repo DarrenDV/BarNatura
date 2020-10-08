342c33915d09b98e30307afc95de8247110d7c7a
G-mecha <maydayfan11@gmail.com>
2020-10-08 11:02:23 +0200

    Added base building script

---
+++ Assets/Scripts/Buildings/BuildingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingScript : MonoBehaviour
+{
+    [Header("Default building variables")]
+    [Tooltip("How much oxygen does this building produce?")]
+    [SerializeField] protected float oxygenProduction = 1f;
+    [Tooltip("How much oxygen does this building drain?")]
+    [SerializeField] protected float oxygenUsage = 1f;
+
+    [Tooltip("How much oxygen does this building polution?")]
+    [SerializeField] protected float polution = 1f;
+
+    [Tooltip("How many humans can be in this building at the same time?")]
+    [SerializeField] protected int maxCapacity;
+    protected int currentCapacity;
+
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+}
