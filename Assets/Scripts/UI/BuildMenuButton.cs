f98c77615fd485f2b72b1983ca1868615630a8da
G-mecha <maydayfan11@gmail.com>
2020-10-08 12:30:50 +0200

    Added basic build menu

---
+++ Assets/Scripts/UI/BuildMenuButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildMenuButton : MonoBehaviour
+{
+    [SerializeField] private BuildMenuItem buildMenuItem;
+    [SerializeField] private Image buttonImage;
+    private GameObject itemToBuild;
+
+    private void Start()
+    {
+        buttonImage.sprite = buildMenuItem.sprite;
+        itemToBuild = buildMenuItem.gameObject;
+    }
+
+    public void OnClick()
+    {
+        //Insert build code here dummy
+    }
+
+
+}
---
+++ Assets/Scripts/UI/BuildMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Build Menu Item", order = 1)]
+public class BuildMenuItem : ScriptableObject
+{
+    public Sprite sprite;
+    public GameObject gameObject;
+}
