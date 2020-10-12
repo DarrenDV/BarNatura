fc8e160f3eddc59b068961fe24aba5cf0d5e7649
Omega_The_III <jeroen.officier@acda.nl>
2020-10-12 14:54:13 +0200

    Camera scripts in place.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,31 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-
+﻿using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject target;
 
-    float speed = 3.5f;
+    float cameraSlowDown = 0.35f;
+    float cameraSlowDownMinimumSpeed = 0.03f;
+    float cameraSlowDownFactor = 0.99f;
+
+
+    float speed = 4.2f;
+    float sensitivity = 5f;
     float minFov = 35f;
     float maxFov = 100f;
-    float sensitivity = 23f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    float mouseX, mouseY;
+
     void Update()
     {
-        // Movement function
-        if (Input.GetMouseButton(1))
+        // When mouse is pressed the cameraSlowDown gets reset and the mouse gets tracked.
+        if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(target.transform.position, transform.up, Input.GetAxis("Mouse X") * speed);
-            transform.RotateAround(target.transform.position, transform.right, Input.GetAxis("Mouse Y") * -speed);
+            cameraSlowDown = 0.35f;
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
         }
+
+        // This takes care of the sliding effect.
+        if (!Input.GetMouseButton(0))
+        {
+            cameraSlowDown *= cameraSlowDownFactor;
+
+            if (cameraSlowDown <= cameraSlowDownMinimumSpeed)
+            {
+                cameraSlowDown = 0;
+            }
+        }
+        // This moves the camera around at all times so that the camera can continue sliding.
+        transform.RotateAround(target.transform.position, transform.up, mouseX * speed * cameraSlowDown);
+        transform.RotateAround(target.transform.position, transform.right, mouseY * -speed * cameraSlowDown);
 
         // Zoom function
         float fov = Camera.main.fieldOfView;
