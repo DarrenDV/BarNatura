dd985f217e48e4ccd0693a0f6bf18803d42b5185
JoppieToppie <topjordy@gmail.com>
2020-10-13 12:28:38 +0200

    Added skybox to main scene

---
+++ Assets/External Assets/HexPlanet/Scripts/NavigationDemo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationDemo : MonoBehaviour
+{
+    public MobileUnit Unit;
+
+    public bool selected = false;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Tile.OnTileClickedAction += OnTileClicked;
+	}
+	
+
+    public void OnTileClicked(Tile tile)
+    {
+        if(!Unit.moving && selected)
+        {
+            Stack<Tile> path;
+            if(Hexsphere.planetInstances[0].navManager.findPath(Unit.currentTile, tile, out path))
+            {
+                Unit.moveOnPath(path);
+            }
+        }
+    }
+}
