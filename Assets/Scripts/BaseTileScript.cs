f8abdefdba8225716439723cfd7d092bdbfe108e
G-mecha <maydayfan11@gmail.com>
2020-11-11 23:55:51 +0100

    - Fixed the player being able to build through the UI - The player can no longer build on polluted tiles - Added the packages map to the gitignore because packages-lock.json is driving me nuts

--- Assets/Scripts/BaseTileScript.cs
+++ Assets/Scripts/BaseTileScript.cs
@@ -82,6 +82,7 @@ public class BaseTileScript : Tile
             return false;
         }
     }
+
     public bool NatureLevelCheck()
     {
         if (naturePercentage >= maxNaturePercentage)
@@ -96,9 +97,11 @@ public class BaseTileScript : Tile
 
     private void OnMouseEnter()
     {
+        //Get the game manager
         if (gameManager == null) gameManager = FindObjectOfType<GameManager>();
         if (gameManager.buildObject != null)
         {
+            //Set the position of the preview building to this tile
             gameManager.buildObjectPreview.gameObject.GetComponent<BuildingModeObject>().ChangeMaterial(isOccupied);
             gameManager.previewObjectParent.transform.position = transform.position;
             gameManager.previewObjectParent.transform.rotation = transform.rotation;
@@ -107,10 +110,10 @@ public class BaseTileScript : Tile
 
     private void OnMouseOver()
     {
-
+        //On build when the game is in the build state
         if (gameManager.buildObject != null)
         {
-            if (Input.GetMouseButtonDown(0) && !isOccupied)
+            if (Input.GetMouseButtonDown(0) && !isOccupied && naturePollutedDegree >= 0 && gameManager.IsPointerOverUIElement() == false)
             {
                 //Place the new building
                 placeObject(Instantiate(gameManager.buildObject.gameObject, transform.position, transform.rotation));
@@ -123,4 +126,5 @@ public class BaseTileScript : Tile
             }
         }
     }
+
 }
