09dd955958d4248f0e3be67699715826e52dec16
Omega_The_III <jeroen.officier@acda.nl>
2020-11-12 12:14:35 +0100

    Rubble tile and spawning complete.

--- Assets/Scripts/BaseTileScript.cs
+++ Assets/Scripts/BaseTileScript.cs
@@ -27,12 +27,20 @@ public class BaseTileScript : Tile
 
     #endregion
 
+    public GameObject RubbleTile;
+
     private GameManager gameManager;
     [HideInInspector]public bool isOccupied;
 
-    void Start(){
-        if(Random.Range(0, 100) == 0){
+    void Start()
+    {
+        if (Random.Range(0, 100) == 0)
+        {
             naturePollutedDegree = -10;
+        }
+        if (Random.Range(0, 100) < 80)
+        {
+            placeObject(Instantiate(RubbleTile, Vector3.zero, Quaternion.identity) );
         }
     }
 
