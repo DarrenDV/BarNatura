8374937078bb38551b8982c048fe134021802200
Omega_The_III <jeroen.officier@acda.nl>
2020-11-12 10:44:29 +0100

    Tree's generate oxygen (REQUIRES GLOBAL OXYGEN)

---
+++ Assets/Scripts/Buildings/TreeTileScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTileScript : MonoBehaviour
+{
+    float treeOxygenProduction = 8;
+    void Start()
+    {
+        // This adds the base production value of 1 tree to the Oxygen Production
+        //Global.OxygenProduction += treeOxygenProduction
+    }
+}
