9bfbcbbebcf501d3c790cb03836d9fc40d2a6f38
G-mecha <maydayfan11@gmail.com>
2020-11-15 02:01:50 +0100

    Changed the house building

--- Assets/Scripts/Buildings/BuildingModeObject.cs
+++ Assets/Scripts/Buildings/BuildingModeObject.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class BuildingModeObject : MonoBehaviour
 {
 
-    [SerializeField] private MeshRenderer[] meshRenderers;
+    //[SerializeField] private MeshRenderer[] meshRenderers;
+    [SerializeField] private MeshRenderer meshRenderer;
     [SerializeField] private Color[] colors;
 
     // Start is called before the first frame update
@@ -24,11 +25,21 @@ public class BuildingModeObject : MonoBehaviour
     public void ChangeMaterial(bool isOccupied)
     {
 
-        for (int i = 0; i < meshRenderers.Length; ++i)
-        {
-            if (isOccupied == false) meshRenderers[i].sharedMaterial.SetColor("_Color", colors[0]);
-            else meshRenderers[i].sharedMaterial.SetColor("_Color", colors[1]);
-        }
+        //for (int i = 0; i < meshRenderers.Length; ++i)
+        //{
+        //    if (isOccupied == false) meshRenderers[i].sharedMaterial.SetColor("_Color", colors[0]);
+        //    else meshRenderers[i].sharedMaterial.SetColor("_Color", colors[1]);
+        //}
+
+        if (isOccupied == false) meshRenderer.sharedMaterial.SetColor("_Color", colors[0]);
+        else meshRenderer.sharedMaterial.SetColor("_Color", colors[1]);
+
+        //for (int i = 0; i < meshRenderer.materials.Length; ++i)
+        //{
+        //    if (isOccupied == false) meshRenderer.sharedMaterial[0].SetColor("_Color", colors[0]);
+        //    else meshRenderer.sharedMaterial[i].SetColor("_Color", colors[1]);
+        //}
+
 
     }
 }
