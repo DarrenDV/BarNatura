7642612ecddb37693997c9f99fc27e8d9d3973b5
JoppieToppie <topjordy@gmail.com>
2020-11-17 13:34:44 +0100

    Added popup

---
+++ Assets/Scripts/Rubble.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Rubble : BaseObject
+{
+    public int RawMaterialMin;
+    public int RawMaterialMax;
+
+    public override string GetName()
+    {
+        return "Rubble";
+    }
+
+    public override string GetDescription()
+    {
+        return $"Remove this rubble to gain {RawMaterialMin} to {RawMaterialMax} raw material.";
+    }
+
+    public override void Remove()
+    {
+        GameManager.Instance.AddRawMaterial(Random.Range(RawMaterialMin, RawMaterialMax));
+
+        base.Remove();
+    }
+}
---
+++ Assets/Scripts/UI/Popup.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    public static Popup Instance;
+
+    public Vector3 DisplayOffset;
+
+    private BaseObject selectedObject;
+
+    [SerializeField] private Text titleText;
+    [SerializeField] private Text descriptionText;
+
+    public void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Popup instance already set!");
+        }
+
+        Hide();
+    }
+
+    public void Show(BaseObject objectToDisplay)
+    {
+        // don't show when already open
+        if (isActiveAndEnabled)
+        {
+            return;
+        }
+
+        selectedObject = objectToDisplay;
+
+        titleText.text = selectedObject.GetName();
+        descriptionText.text = selectedObject.GetDescription();
+
+        transform.position = Camera.main.WorldToScreenPoint(selectedObject.transform.position) + DisplayOffset;
+
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnRemoveClicked()
+    {
+        selectedObject.Remove();
+        Hide();
+    }
+}
