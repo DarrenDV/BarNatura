99325f6a6a73b4a7bd2c9256e9d5b5da03595941
JoppieToppie <topjordy@gmail.com>
2020-11-18 09:45:18 +0100

    5 times the detail
    300% the fps!!
    4 times the scale

--- Assets/Scripts/BaseTileScript.cs
+++ Assets/Scripts/BaseTileScript.cs
@@ -6,17 +6,19 @@ public class BaseTileScript : Tile
 {
 
     #region Spreading Variables
+
     [Header("Tile pollution state variables")]
     protected float maxPollutedPercentage = 100f;
+
     [SerializeField] protected float pollutedPercentage;
 
     [Header("Tile nature state variables")]
     protected float maxNaturePercentage = 100f;
+
     [SerializeField] protected float naturePercentage;
 
-    [Tooltip("The degree to which a tile is either polluted or nature")]
-    [Range(-10, 10)]
-    public int naturePollutedDegree = 0;
+    [Tooltip("The degree to which a tile is either polluted or nature")] [Range(-10, 10)]
+    public int naturePollutedDegree; // make property so it updates the materials automatic
 
     float timerSpread;
     [SerializeField] float secondsToUpdate;
@@ -29,17 +31,23 @@ public class BaseTileScript : Tile
     public GameObject lavaTile;
     [SerializeField] int lavaChance;
 
+    private MeshRenderer meshRenderer;
+    private bool doMaterialUpdate;
+
     protected override void Start()
     {
         base.Start();
 
+        meshRenderer = GetComponent<MeshRenderer>();
 
         int rubbleSpawnChange = 20;
 
         if (Random.Range(0, 100) == 0)
         {
             naturePollutedDegree = -10;
+            doMaterialUpdate = true;
         }
+
         if (Random.Range(0, 100) < rubbleSpawnChange)
         {
             placeObject(Instantiate(RubbleTile, Vector3.zero, Quaternion.identity));
@@ -52,52 +60,72 @@ public class BaseTileScript : Tile
         }
     }
 
-    protected override void Update(){
+    protected override void Update()
+    {
         base.Update();
         Spread();
     }
 
     #region Tile Spreading
 
-    void Spread(){
+    void Spread()
+    {
+        if (timerSpread >= secondsToUpdate)
+        {
+            foreach (var tile in neighborTiles)
+            {
+                var neighbour = (BaseTileScript) tile;
 
-        if(timerSpread >= secondsToUpdate){
-
-            foreach(BaseTileScript neighbour in neighborTiles){
                 //Only applies the spreading if the random is met.
-                if(Random.Range(0,4) == 3){
+                if (Random.Range(0, 4) == 0)
+                {
                     ToxicSpreading(neighbour);
                     NatureSpreading(neighbour);
                 }
             }
-            
+
             timerSpread = 0;
         }
+
+        //if (naturePollutedDegree != 0)
+        //{
         timerSpread += Time.deltaTime;
+        //}
 
-        MeshRenderer mesh = GetComponent<MeshRenderer>();
-        mesh.material.SetColor("_Color", gradient.Evaluate(Map(naturePollutedDegree, -10, 10, 0, 1)));
-    }
-
-    void ToxicSpreading(BaseTileScript neighbour){
-            
-        if(neighbour.naturePollutedDegree == -10 && naturePollutedDegree > -10){
-            naturePollutedDegree--;
-
+        if (doMaterialUpdate)
+        {
+            meshRenderer.material.SetColor("_Color", gradient.Evaluate(Map(naturePollutedDegree, -10, 10, 0, 1)));
+            doMaterialUpdate = false;
         }
-        if(naturePollutedDegree == -10) canBecomeNature = false;
     }
 
-    void NatureSpreading(BaseTileScript neighbour){
+    void ToxicSpreading(BaseTileScript neighbour)
+    {
 
-        if(neighbour.naturePollutedDegree == 10 && naturePollutedDegree < 10 && canBecomeNature){
+        if (neighbour.naturePollutedDegree == -10 && naturePollutedDegree > -10)
+        {
+            naturePollutedDegree--;
+            doMaterialUpdate = true;
+        }
+
+        if (naturePollutedDegree == -10)
+        {
+            canBecomeNature = false;
+        }
+    }
+
+    void NatureSpreading(BaseTileScript neighbour)
+    {
+        if (neighbour.naturePollutedDegree == 10 && naturePollutedDegree < 10 && canBecomeNature)
+        {
             naturePollutedDegree++;
+            doMaterialUpdate = true;
         }
     }
 
     #endregion
 
-    public float Map (float value, float fromSource, float toSource, float fromTarget, float toTarget)
+    public float Map(float value, float fromSource, float toSource, float fromTarget, float toTarget)
     {
         return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
     }
@@ -146,6 +174,7 @@ public class BaseTileScript : Tile
     private void OnMouseOver()
     {
         var gameManager = GameManager.Instance;
+
         if (gameManager.buildObject != null)
         {
             if (Input.GetMouseButtonDown(0) && !isOccupied && naturePollutedDegree >= 0 && gameManager.IsPointerOverUIElement() == false)
@@ -154,9 +183,10 @@ public class BaseTileScript : Tile
                 placeObject(Instantiate(gameManager.buildObject.gameObject, transform.position, transform.rotation));
                 gameManager.ChangeBuildingMaterial(gameManager.buildObject.gameObject.GetComponent<BuildObject>().buildCost);
 
-                if(gameManager.buildObject.gameObject.CompareTag("Tree"))
+                if (gameManager.buildObject.gameObject.CompareTag("Tree"))
                 {
                     naturePollutedDegree = 10;
+                    doMaterialUpdate = true;
                 }
 
                 gameManager.StopBuilding();
