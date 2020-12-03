16a136f525366fff15897096afd93d25665dc2fb
Omega_The_III <jeroen.officier@acda.nl>
2020-12-03 11:42:21 +0100

    Atmosphere code update

--- Assets/Scripts/AtmosphereSystem.cs
+++ Assets/Scripts/AtmosphereSystem.cs
@@ -2,32 +2,36 @@
 
 public class AtmosphereSystem : MonoBehaviour
 {
-    protected int atmosphereLevel1Threshhold = 1300, atmosphereLevel2Threshhold = 2600, atmosphereLevel3Threshhold = 3900;
-    public int currentOxygen, currentAtmosphereLevel;
-    protected float atmosphereTimer = 0;
+    [SerializeField] private int atmosphereLevel1Threshhold = 1300, atmosphereLevel2Threshhold = 2600, atmosphereLevel3Threshhold = 3900;
+    [SerializeField] private float maxAtmpshereTimer = 5;
+    private int currentOxygen, currentAtmosphereLevel;
+    private float atmosphereTimer = 0;
     // Update is called once per frame
     void Update()
     {
-        //TIMER IS NOT OBLIGATED, REMOVE IF NEED BE.
         //This counts up the total amount of oxygen produced by the player.
-        if (atmosphereTimer * Time.deltaTime > 5)
+        if (atmosphereTimer > maxAtmpshereTimer)
         {
             currentOxygen += GameManager.Instance.GetOxygenSurplus();
+            LevelCheck();
+            atmosphereTimer = 0;
         }
-
+        atmosphereTimer += Time.deltaTime;
+    }
+    void LevelCheck()
+    {
         //level check, this could be a switch case but i cant fill in atmosphereLevel1Threshhold as a case, with no magic numbers this is all i could think of.
+        if (currentOxygen > atmosphereLevel3Threshhold)
+        {
+            currentAtmosphereLevel = 3;
+        } else
+        if (currentOxygen > atmosphereLevel2Threshhold)
+        {
+            currentAtmosphereLevel = 2;
+        } else
         if (currentOxygen > atmosphereLevel1Threshhold)
         {
             currentAtmosphereLevel = 1;
         }
-        if (currentOxygen > atmosphereLevel2Threshhold)
-        {
-            currentAtmosphereLevel = 2;
-        }
-        if (currentOxygen > atmosphereLevel3Threshhold)
-        {
-            currentAtmosphereLevel = 3;
-        }
-        atmosphereTimer++;
     }
 }
