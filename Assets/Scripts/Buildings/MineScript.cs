453e7c53ef9ab758b51215379de0d48426849843
Omega_The_III <jeroen.officier@acda.nl>
2020-12-08 15:37:08 +0100

    Mine Building plus button added.

---
+++ Assets/Scripts/Buildings/MineScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineScript : BuildingScript
+{
+    public ParticleSystem particleSystem;
+
+    [SerializeField] private int rawMaterialsOverTime = 1;
+    [SerializeField] private float maxMineTimer = 18;
+
+    private float mineTimer;
+    
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if (mineTimer > maxMineTimer && !IsBeingBuild && !IsBeingRemoved)
+        {
+            GameManager.Instance.AddRawMaterial(rawMaterialsOverTime);
+            mineTimer = 0;
+        }
+        mineTimer += Time.deltaTime;
+    }
+
+    public override string GetName()
+    {
+        return "Mine";
+    }
+
+    public override string GetDescription()
+    {
+        return $"This mine slowly produces raw materials over time";
+    }
+    public override void OnFinishedRemoving()
+    {
+        base.OnFinishedRemoving();
+
+        transform.parent.GetComponent<BaseTileScript>().DeletePlacedObjects();
+    }
+
+    public override void OnFinishedBuilding()
+    {
+        base.OnFinishedBuilding();
+        particleSystem.Play();
+    }
+}
