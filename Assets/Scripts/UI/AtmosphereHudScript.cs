3ed6d5260d32736c2244235d69b010e48f65f477
Omega_The_III <jeroen.officier@acda.nl>
2020-12-14 16:12:35 +0100

    Added comments and final touch ups.

--- Assets/Scripts/UI/AtmosphereHudScript.cs
+++ Assets/Scripts/UI/AtmosphereHudScript.cs
@@ -6,6 +6,7 @@ public class AtmosphereHudScript : MonoBehaviour
 {
     [SerializeField] private Vector3 extendedPosition;
     [SerializeField] private float extentionSpeed = 1;
+
     private bool isOpen = false;
     private Vector3 basePosition;
     private void Start()
@@ -16,6 +17,7 @@ public class AtmosphereHudScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This animates the pop-up depending on the current desired location (extended or closed).
         if (isOpen)
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, extendedPosition, Time.deltaTime * extentionSpeed);
@@ -25,6 +27,8 @@ public class AtmosphereHudScript : MonoBehaviour
             transform.localPosition = Vector3.Lerp(transform.localPosition, basePosition, Time.deltaTime * extentionSpeed);
         }
     }
+
+    //Toggles the popup to be open or closed.
     public void Toggle()
     {
         isOpen = !isOpen;
