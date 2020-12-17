7be760ada440145b092bd7c63af241e34ee69231
DarrenDV <darrendevre@hotmail.com>
2020-12-17 11:55:35 +0100

    Build cost on return moved to BuildObject script, now adds resources when demolition is finished.

--- Assets/Scripts/Buildings/BuildObject.cs
+++ Assets/Scripts/Buildings/BuildObject.cs
@@ -8,8 +8,19 @@ public class BuildObject : OxygenUser
     [Tooltip("The minimum health a tile must have bafore this building can be placed on it")]
     public int MinimumNaturePollutedDegree = 1;
 
+    [Tooltip("Divides the build cost by this int. How much materials are returned on demolish.")]
+    [SerializeField] int buildCostReturnDivider = 2;
+
     public override string GetBuildDescription()
     {
         return $"<b>Build cost:</b>\nBuilding materials: {BuildCost}\nHumans required: {HumansRequiredToBuild}\nMinimum tile health: {MinimumNaturePollutedDegree}";
+    }
+
+    public override void OnFinishedRemoving()
+    {
+        base.OnFinishedRemoving();
+
+        //Gives resources back
+        GameManager.Instance.AddBuildingMaterial(BuildCost / buildCostReturnDivider);
     }
 }
--- Assets/Scripts/Buildings/BuildingScript.cs
+++ Assets/Scripts/Buildings/BuildingScript.cs
@@ -7,8 +7,7 @@ public class BuildingScript : BuildObject
     [Tooltip("How many humans can be in this building at the same time?")]
     [SerializeField] protected int maxCapacity;
 
-    [Tooltip("How much materials are returned on demolish, divides the build cost by this int.")]
-    [SerializeField] int buildCostReturn = 2;
+
 
     private List<Human> occupants = new List<Human>();
 
@@ -23,9 +22,6 @@ public class BuildingScript : BuildObject
     public override void OnRemove()
     {
         base.OnRemove();
-
-        //Gives resources back
-        GameManager.Instance.AddBuildingMaterial(BuildCost / buildCostReturn);
 
         // testing
         GameManager.Instance.BuildingCount--;
