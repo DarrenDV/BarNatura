ff9cc61dc3a2dae00853137da8692c8c33365dde
DarrenDV <darrendevre@hotmail.com>
2020-12-21 14:22:27 +0100

    Added a pollution converter, icon missing as of now

---
+++ Assets/Scripts/Buildings/PollutionConverter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollutionConverter : BuildingScript
+{
+
+    #region Variables
+
+    [Header("Pollution Converter")]
+    [SerializeField] private int pollutionToRemove;
+    [SerializeField] private int oxygenToAdd;
+
+    //Conversion timer variables
+    [Tooltip("Time in seconds for every conversion")]
+    [SerializeField] private float conversionRate;
+    private float conversionTimer;
+
+    #endregion
+
+    #region Strings
+
+    public override string GetName()
+    {
+        return "Toxic cleaner";
+    }
+
+    protected override string GetBuildingFunction()
+    {
+        return $"Converts {HudManager.GetIcon("Pollution")} into {HudManager.GetIcon("OxygenPlus")}";
+    }
+
+    public override string GetDescription()
+    {
+        return $"This pollution converter converts {pollutionToRemove} {HudManager.GetIcon("Pollution")} into {oxygenToAdd} every {conversionRate} seconds.";
+    }
+
+    #endregion
+
+    #region Default
+
+    protected override void Update()
+    {
+        base.Update();
+
+        UpdatePollution();
+    }
+
+    /// <summary>
+    /// Updates pullution in the atmosphere using the pollution converter.
+    /// </summary>
+    private void UpdatePollution()
+    {
+        if (!IsBeingBuild && !IsBeingRemoved)
+        {
+            if(GameManager.Instance.GetPollution() > 0)
+            {
+                conversionTimer += Time.deltaTime;
+
+                if (conversionTimer >= conversionRate)
+                {
+                    GameManager.Instance.RemovePollution(pollutionToRemove);
+                    GameManager.Instance.AddOxygenGeneration(oxygenToAdd);
+
+                    conversionTimer = 0;
+                }
+            }
+        }
+    }
+
+    #endregion
+}
